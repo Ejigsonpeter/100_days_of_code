28fd795d28c7802361880b876c6dbe46490d9ca3
ejigsonpeter <ejigsonpeter@gmail.com>
2024-01-03 23:59:34 -0500

    grind

---
+++ day 2/Manipulation_fstrings.py
@@ -0,0 +1,2 @@
+print(round((8/3),2))  #round up
+print(4//3) #floor division gives absolute val in integer
---
+++ day 2/mathsops.py
@@ -0,0 +1,12 @@
+3 + 5
+6/ 3
+4 + 4
+print(type(6/2.5))
+#power 
+print(2**3)
+
+#PEMDAS : Parenthesis, Exponents, Multiplication, Division, Addition and Subtraction
+
+
+print (3 * 3 + 3 / 3-3) #in this case the code prioritize the leftmost operator between / and *
+
