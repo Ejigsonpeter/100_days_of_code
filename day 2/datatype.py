51a85b853307abe34435c8668b2dd7f8562f7241
ejigsonpeter <ejigsonpeter@gmail.com>
2024-01-03 23:27:28 -0500

    steady and grinding day3

--- day 2/datatype.py
+++ day 2/datatype.py
@@ -1,1 +1,17 @@
-data
+#data type
+
+#string
+a = print ("hello"[0]) #subscripting
+
+#integer
+b = print(123 + 567)
+# 457,890 are same as 457_890
+
+#floating point number
+
+print(8.090)
+
+#Bolean  
+
+True 
+False
---
+++ day 2/typechecking.py
@@ -0,0 +1,12 @@
+num_char = len(input("what is your name?"))
+print(type(num_char)) #check type
+#print ("your name has "+ num_char+ " characters")
+
+#ype conversion or casting
+
+new_num_char = str(num_char)
+
+print ("your name has "+ new_num_char+ " characters")
+
+a = float(123)
+print(type(a))
