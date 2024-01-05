29079b6eaaad07e46e3c64ed8533cd3fe5be6212
ejigsonpeter <ejigsonpeter@gmail.com>
2024-01-04 22:37:17 -0500

    day 4 - still going

---
+++ day 4/random_example.py
@@ -0,0 +1,18 @@
+import random
+import my_module
+
+
+print(my_module.pi)
+
+#generate randome integers
+random_integer = random.randint(1, 10)
+print(random_integer)
+
+#generate floating number 
+random_float = random.random() * 5
+print(random_float)
+
+
+love_score = random.randint(1,10)
+print(f"Your love score is {love_score}")
+
